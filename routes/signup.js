8ed299d2d6686b60386fdc3dcd5950e37a4b275b
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-15 02:12:13 +0300

    validate new user

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,7 @@
 const Router = require('express').Router()
 const path = require('path')
+const mongoose = require('mongoose')
+const userModel = require('../models/user')
 
 Router.get('/signup', (req, res) => {
     const PATH_SIGNUP = path.join(__dirname, '..', 'public', 'signup.html')
@@ -7,11 +9,36 @@ Router.get('/signup', (req, res) => {
     res.sendFile(PATH_SIGNUP)
 })
 
-Router.post('/signup', (req, res) => {
-    const { username, password} = req.body
-    console.log(username, password)
-
-    res.send('1')
+Router.post('/signup', validateUser, (req, res) => {
+    // if the user is verified
+    // we redirect on the chat page
+    res.redirect('/chat')
 })
+
+//performs user verification 
+async function validateUser(req, res, next) {
+    const { username, password} = req.body
+
+    if(!username || !password) {
+        return res.sendStatus(401)
+    }
+
+    const isUserExist = ( await userModel.find({ username, password }) ).length !== 0
+    if(isUserExist) {
+        return res.sendStatus(403)
+    }
+
+    // add user to DB
+    new userModel({ username, password }).save((err, doc) => {
+        if(err) {
+            res.sendStatus(500)
+            throw err;
+        }
+
+        console.log(doc)
+    })
+
+    next()
+}
 
 module.exports = Router
--- schemas/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-let userSchema = new mongoose.Schema(
-    {username: String, password: String},
-    { timestamps: { createdAt: 'created_at' } }
-)
-

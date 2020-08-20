0b4f95c10729ddc94fca789000568366046cfcc4
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-21 01:49:18 +0300

    path room

--- public/createRoom.js
+++ public/createRoom.js
@@ -1,13 +1,14 @@
 create_room.addEventListener('click', () => {
     let roomName = prompt('room name?')
     if(roomName === null || roomName === '') return alert('Incorrect input');
+    let { _id: user_id } = JSON.parse( localStorage.user )
 
-    fetch('http://localhost:8080/room/create', {
+    fetch('http://localhost:8080/chat/room/create', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({roomName})
+        body: JSON.stringify({ roomName, users: [user_id] })
     })
     .then( async (res) => {
         let roomObj = await res.json()

7426f8fca2b09c599b725b315bceafe340556063
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-21 01:36:43 +0300

    handler rooms

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -1,17 +1,37 @@
 const Router = require('express').Router()
 const path = require('path')
+const { nanoid } = require('nanoid')
+// room storage 
+let rooms = new Map()
 
-/* /room/create */
 Router.post('/create', (req, res) => {
-    res.json(req.body)
+    const room_id = nanoid(), roomObj = { room_id, ...req.body } 
+    rooms.set(room_id, roomObj)
+
+    res.json(roomObj)
 })
 
 Router.get('/:id', (req, res) => {
-    res.json(req.params)
+    let room_id = req.params.id
+
+    if(rooms.has(room_id)) {
+        res.json(rooms.get(room_id))
+    }
+    else {
+        res.status(401).send('Unauthorized')
+    }
 })
 
 Router.delete('/delete', (req, res) => {
-    res.send('...')
+    let room_id = req.body.room_id
+
+    let isRoomDeleted = rooms.delete(room_id)
+    if(isRoomDeleted) {
+        res.sendStatus(200)
+    }
+    else {
+        res.status(401).send('Unauthorized')
+    }
 })
 
 module.exports = Router

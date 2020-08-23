50fff2764c43166fcc033a5d00b8d32040ded953
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-23 20:39:00 +0300

    server port

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,6 +8,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './build'
+        contentBase: './build',
+        port: 6969
     }
 })

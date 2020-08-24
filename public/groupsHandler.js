1c8b00708a47aae3df2093d13a33af32dbce5bca
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-24 18:35:15 +0300

    error processign

--- public/groupsHandler.js
+++ public/groupsHandler.js
@@ -11,6 +11,10 @@ function createRoomHandler() {
         body: JSON.stringify({ roomName, users: [user_id] })
     })
     .then( async (res) => {
+        if(res.status !== 200) {
+            return alert(`Status: ${res.status} ${res.statusText}`)
+        }
+
         let roomObj = await res.json()
         console.log(roomObj)
     })
@@ -19,15 +23,22 @@ function createRoomHandler() {
 function joinToGroup() {
     let room_id = prompt('room id?')
     if(room_id === null || room_id === '') return alert('Incorrect input');
+    let { _id: user_id} = JSON.parse( localStorage.user )
 
     fetch('http://localhost:8080/chat/room/join', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ room_id })
+        body: JSON.stringify({ user_id, room_id })
     })
     .then( async (res) => {
+        if(res.status !== 200) {
+            return res.status === 400 ?
+                alert(`Status: ${res.status} ${res.statusText}\nUser already exist`) :
+                alert(`Status: ${res.status} ${res.statusText}`)
+        }
+
         let roomObj = await res.json()
         console.log(roomObj)
     })

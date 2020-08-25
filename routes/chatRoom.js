fe84bda0e8779316f5bf79fe9dc422386cdf37f5
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-26 02:41:52 +0300

    connected the database

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -1,20 +1,29 @@
 const Router = require('express').Router()
 const { nanoid } = require('nanoid')
 const path = require('path')
+const roomModel = require('../models/room')
 // room storage 
 let rooms = new Map()
 
 Router.post('/create', (req, res) => {
     const room_id = nanoid(), roomObj = { room_id, ...req.body } 
-    rooms.set(room_id, roomObj)
 
-    res.json(roomObj)
+    new roomModel(roomObj).save((err, doc) => {
+        if(err) {
+            res.status(500).send('Internal Server Error')
+            throw err;
+        }
+
+        res.json(roomObj)
+    })
 })
 
-Router.get('/', (req, res) => {
-    let room_id = req.query.id
+Router.get('/', async (req, res) => {
+    let room_id = req.query.id  
 
-    if(rooms.has(room_id)) {
+    const isRoomExist = ( await roomModel.find({ room_id })).length !== 0
+
+    if(isRoomExist) { 
         process.env.NODE_ENV === 'production' ? 
             res.sendFile(path.join(__dirname, '..', 'build', 'chatGroup.html')) :
             res.sendFile(path.join(__dirname, '..', 'public', 'chatGroup.html'))
@@ -24,47 +33,54 @@ Router.get('/', (req, res) => {
     }
 })
 
-Router.post('/', (req, res) => {
+Router.post('/', async (req, res) => {
     let { room_id } = req.body
 
-    if(rooms.has(room_id)) {
-        res.json( rooms.get(room_id) )
+    let room = ( await roomModel.find({ room_id }))[0]
+    const isRoomExist = !!room
+
+    if(isRoomExist) {
+        res.json( room )
     }
     else {
         res.status(401).send('Unauthorized')
     }
 })
 
-Router.post('/join', (req, res) => {
+Router.post('/join', async (req, res) => {
     let { user_id, room_id } = req.body
-    let room = rooms.get(room_id)
+    let room = ( await roomModel.find({ room_id }))[0]
 
     if(room === undefined) {
         return res.status(401).send('Unauthorized')
     }
-    else if ( alreadyHaveUser(user_id, room_id) ) {
+    else if ( alreadyHaveUser(user_id, room) ) {
         return res.status(400).send('BAD REQUEST')
     }
-    room.users.push(user_id)
 
-    res.json(room)
+    room.users.push(user_id) 
+    roomModel.updateOne({ room_id }, { users: room.users }, (err, doc) => {
+        if(err) {
+            res.status(500).send('Internal Server Error')
+            throw err;
+        }
+        res.json( room )
+    })
 })
 
-function alreadyHaveUser(user_id, room_id) {
-    let room = rooms.get(room_id)
+function alreadyHaveUser(user_id, room) {
     return room.users.includes(user_id)
 }
 
 Router.delete('/delete', (req, res) => {
     let room_id = req.body.room_id
 
-    let isRoomDeleted = rooms.delete(room_id)
-    if(isRoomDeleted) {
+    roomModel.deleteOne({ room_id }, (err) => {
+        if(err) {
+            return res.status(401).send('Unauthorized')
+        }
         res.sendStatus(200)
-    }
-    else {
-        res.status(401).send('Unauthorized')
-    }
+    })
 })
 
 module.exports = Router

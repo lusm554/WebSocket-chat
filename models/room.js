61d449c33511641ba44bd67d8fd819c07995ee32
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-26 01:03:54 +0300

    model room

--- models/room.js
+++ models/room.js
@@ -4,5 +4,5 @@ let roomSchema = new mongoose.Schema(
     { name: String, room_id: String, users: [String] },
     { timestamps: { createdAt: 'created_at' } }
 )
-let roomModel = mongoose.model('Users', roomSchema)
+let roomModel = mongoose.model('Rooms', roomSchema)
 module.exports = roomModel

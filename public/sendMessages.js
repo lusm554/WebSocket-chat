5771f40cfa94f77f413b9811e8fb0014630f349f
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-26 15:11:44 +0300

    get user function

--- public/sendMessages.js
+++ public/sendMessages.js
@@ -1,3 +1,8 @@
+/**
+ * Send message to the server from common chat
+ * @param {Object} msg - object with information about user and message
+ * @param {*} ws 
+ */
 function sendMessageToServer(msg, ws) {
     let jsonMsg = JSON.stringify(msg)
     ws.send(jsonMsg)
@@ -5,4 +10,12 @@ function sendMessageToServer(msg, ws) {
     m.value = ''
 }
 
-export { sendMessageToServer }
+
+function getUser() {  
+    const USER = JSON.parse(
+        localStorage.getItem('user')
+    )
+    return USER;
+}
+
+export { sendMessageToServer, getUser }
